6338816bfd3a2a3ca39b0caab4db4404d42b7baf
praneeth reddy <48400471+PraneethReddy-N@users.noreply.github.com>
2023-09-06 13:09:50 -0400

    Lift Simulation files

---
+++ .idea/LiftSimulation.js
@@ -0,0 +1,9 @@
+var LiftCount = document.getElementById("Lift");
+var FloorCount = document.getElementById("TotalCount");
+var button = document.getElementById("Submit");
+
+button.onclick = function(){
+   if(LiftCount && FloorCount !=0){
+         alert("yestst");
+   }
+}
